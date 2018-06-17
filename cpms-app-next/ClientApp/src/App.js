ae2b59b828767085edab0e10e1ae485ccba84913
Jitendra Prajapati <prajapatijv@hotmail.com>
2018-06-17 20:06:29 +1000

    Added Stub for Users.

--- cpms-app-next/ClientApp/src/App.js
+++ cpms-app-next/ClientApp/src/App.js
@@ -4,11 +4,13 @@ import Layout from './components/Layout';
 import Home from './components/Home';
 import Counter from './components/Counter';
 import FetchData from './components/FetchData';
+import Users from './components/user/users'
 
 export default () => (
   <Layout>
     <Route exact path='/' component={Home} />
     <Route path='/counter' component={Counter} />
     <Route path='/fetchdata/:startDateIndex?' component={FetchData} />
-  </Layout>
+    <Route path='/users' component={Users} />
+</Layout>
 );
---
+++ cpms-app-next/ClientApp/src/components/user/users.js
@@ -0,0 +1,20 @@
+﻿import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+//import { actionCreators } from '../store/WeatherForecasts';
+
+class Users extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Manage User</h1>
+                <p>This component demonstrates fetching data from the server and working with URL parameters.</p>
+            </div>
+        );
+    }
+}
+
+
+export default Users;
