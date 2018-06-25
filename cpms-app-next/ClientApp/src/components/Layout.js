ecc881c874d512d9a26272bc320417fdf1a2c52c
Jitendra Prajapati <prajapatijv@hotmail.com>
2018-06-25 19:03:47 +1000

    Updated Side menu.

--- cpms-app-next/ClientApp/src/components/Layout.js
+++ cpms-app-next/ClientApp/src/components/Layout.js
@@ -14,7 +14,7 @@ export default props => (
                             <line x1="14.31" y1="8" x2="20.05" y2="17.94"></line><line x1="9.69" y1="8" x2="21.17" y2="8"></line>
                             <line x1="7.38" y1="12" x2="13.12" y2="2.06"></line><line x1="9.69" y1="16" x2="3.95" y2="6.06"></line><line x1="14.31" y1="16" x2="2.83" y2="16"></line><line x1="16.62" y1="12" x2="10.88" y2="21.94"></line></svg>
                     </a>
-                    <a class="py-2 d-md-inline-block" href="/">Design Group</a>
+                    <a class="py-2 p-done d-md-inline-block" href="/">Design Group</a>
                 </div>
             </nav>
         </header>
--- cpms-app-next/ClientApp/src/components/NavMenu.js
+++ cpms-app-next/ClientApp/src/components/NavMenu.js
@@ -3,17 +3,68 @@ import { NavLink } from 'react-router-dom';
 import { LinkContainer } from 'react-router-bootstrap';
 import './NavMenu.css';
 
+//TODO: Move this to separate folder.
+const MenuData = [
+    {
+        title: "TRANSACTIONS",
+        navItems: [
+            { title: "Dashboard", routeTo: "/" },
+            { title: "Dispatch", routeTo: "/dispatch" },
+            { title: "Return", routeTo: "/return" },
+            { title: "Settlement", routeTo: "/settlement" },
+        ]
+    },
+    {
+        title: "MASTERS",
+        navItems: [
+            { title: "Customers", routeTo: "/customers" },
+            { title: "Items", routeTo: "/items" },
+            { title: "Users", routeTo: "/users" },
+        ]
+    },
+    {
+        title: "REPORTS",
+        navItems: [
+            { title: "Ledger", routeTo: "/ledger-report" },
+            { title: "Customers", routeTo: "/customers-report" },
+            { title: "Items", routeTo: "/items-report" },
+        ]
+    }];
+
 export default props => (
 <div>
     <div className="sidebar-sticky">
-        <ul className="nav flex-column">
-            <li key="home" className="nav-item">
-                <NavLink className="nav-link" to="/" exact >Home</NavLink>
-            </li>
-            <li key="users" className="nav-item">
-                <NavLink className="nav-link" to="/users" exact >Users</NavLink>
-            </li>
-        </ul>
+        <MenuItems navGroups={MenuData} />
     </div>
 </div>
 );
+
+function MenuItems(props) {
+    const listItems =
+        <div>
+            {props.navGroups.map((navGroup) =>
+                <div key={navGroup.title}>
+                    <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+                        <span>{navGroup.title}</span>
+                    </h6>
+                    <NavItem key={navGroup.title + "_item"} navItems={navGroup.navItems} />
+                </div>
+            )}
+        </div>
+
+    return listItems;
+};
+
+function NavItem(props) {
+    const menuItem =
+        <ul className="nav flex-column">
+            {props.navItems.map(navItem =>
+                <li key={navItem.title} className="nav-item">
+                    <NavLink className="nav-link" to={navItem.routeTo} exact activeClassName="active">{navItem.title}</NavLink>
+                </li>
+            )}
+        </ul>
+
+    return menuItem;
+};
+
