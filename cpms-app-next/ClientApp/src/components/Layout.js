093a25056b70da2d2c408556f29a0a786e7077fa
Jitendra Prajapati <prajapatijv@hotmail.com>
2018-06-25 18:49:52 +1000

    Updated Header row.

--- cpms-app-next/ClientApp/src/components/Layout.js
+++ cpms-app-next/ClientApp/src/components/Layout.js
@@ -3,17 +3,24 @@ import NavMenu from './NavMenu';
 
 export default props => (
     <div>
-    <header>
-            <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-            <a className="navbar-brand" href="#">Design Group</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+        <header>
+            <nav class="site-header sticky-top py-1">
+                <div class="container d-flex flex-column flex-md-row justify-content-between">
+                    <a class="py-2" href="/">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                            viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                            stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                            class="d-block mx-auto"><circle cx="12" cy="12" r="10"></circle>
+                            <line x1="14.31" y1="8" x2="20.05" y2="17.94"></line><line x1="9.69" y1="8" x2="21.17" y2="8"></line>
+                            <line x1="7.38" y1="12" x2="13.12" y2="2.06"></line><line x1="9.69" y1="16" x2="3.95" y2="6.06"></line><line x1="14.31" y1="16" x2="2.83" y2="16"></line><line x1="16.62" y1="12" x2="10.88" y2="21.94"></line></svg>
+                    </a>
+                    <a class="py-2 d-md-inline-block" href="/">Design Group</a>
+                </div>
             </nav>
-    </header>
+        </header>
     <div className="container-fluid">
         <div className="row">
-            <nav class="col-md-2 navbar navbar-expand-lg navbar-light bg-light">
+            <nav class="col-md-3 navbar navbar-expand-lg navbar-light bg-light">
                 <NavMenu />
             </nav>
             <main className="col-md-9">
