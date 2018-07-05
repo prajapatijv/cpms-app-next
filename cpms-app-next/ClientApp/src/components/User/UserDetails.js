4adae4460e36bd3f888a2c462909c1b01121bf67
Jitendra Prajapati <prajapatijv@hotmail.com>
2018-07-05 21:10:27 +1000

    Refacotred User Detail presentation component.

---
+++ cpms-app-next/ClientApp/src/components/User/UserDetails.js
@@ -0,0 +1,86 @@
+﻿import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { actionCreators } from '../../store/UserStore';
+
+class UserDetails extends Component {
+    render() {
+        const { user } = this.props;
+
+        return (
+            <div className="row">
+                <UserForm user={user} />
+            </div>
+        );
+    }
+}
+
+const UserForm = ({ user }) => {
+
+    /*
+    handleChange(e) {
+        // If you are using babel, you can use ES 6 dictionary syntax
+        // let change = { [e.target.name] = e.target.value }
+        let change = {}
+        change[e.target.name] = e.target.value
+        this.setState(change)
+        //this.props.dispatch()
+    }*/
+
+    return (
+        (user !== undefined) &&
+        <div className="col-md-6 order-md-1">
+            <h4 className="mb-3">ADD EDIT USER</h4>
+            <hr className="mb-4"></hr>
+            <form className="needs-validation" noValidate>
+                <div className="row">
+                    <div className="col-md-6 mb-3">
+                        <label htmlFor="firstName">First name</label>
+                        <input type="text" className="form-control" name="firstName" placeholder="" value={user.firstName} onChange={(e) => { this.handleChange(e) }} required />
+                        <div className="invalid-feedback">
+                            Valid first name is required.
+                        </div>
+                    </div>
+                    <div className="col-md-6 mb-3">
+                        <label htmlFor="lastName">Last name</label>
+                        <input type="text" className="form-control" name="lastName" placeholder="" value={user.lastName} onChange={() => { }} required />
+                        <div className="invalid-feedback">
+                            Valid last name is required.
+                        </div>
+                    </div>
+                </div>
+
+                <div className="mb-3">
+                    <label htmlFor="username">Username</label>
+                    <div className="input-group">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text">@</span>
+                        </div>
+                        <input type="text" className="form-control" name="username" placeholder="Username" value={user.userName} onChange={() => { }} required />
+                        <div className="invalid-feedback">
+                            Your username is required.
+                        </div>
+                    </div>
+                </div>
+
+                <div className="mb-3">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" className="form-control" id="password" placeholder="Password" />
+                    <div className="invalid-feedback">
+                        Please enter a valid password.
+                    </div>
+                </div>
+                <div className="row  float-right">
+                    <button className="btn btn-outline-primary" type="button">Cancel</button>
+                    <button className="btn btn-outline-primary" type="submit">Save</button>
+                </div>
+            </form>
+        </div>
+    );
+}
+
+
+export default connect(
+    state => state.user,
+    dispatch => bindActionCreators(actionCreators, dispatch)
+)(UserDetails);
