a8c39bdf4487a101b54f7388fd0df405adee0de5
Jitendra Prajapati <prajapatijv@hotmail.com>
2018-07-16 21:42:35 +1000

    Added Repository class.

---
+++ cpms-app-next/Repository/BaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using cpms_app_next.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cpms_app_next.Repository
+{
+    public class BaseRepository<T>
+    {
+    }
+
+    public class UserRepository : BaseRepository<User>
+    {
+
+    }
+}
