715bbe558b689ce5a91a590066e19dfeab7c823c
Jitendra Prajapati <prajapatijv@hotmail.com>
2018-09-05 20:29:51 +1000

    Updated Styles for Selected item.

--- cpms-app-next/ClientApp/src/components/User/UserList.js
+++ cpms-app-next/ClientApp/src/components/User/UserList.js
@@ -3,19 +3,22 @@
 const UserList = ({ users, selectedUser, onSelectUser, onAddUser, onDeleteUser }) => {
     return(
         <div>
-            <h2 className="d-inline">Manage Users</h2>
+            <h4 className="d-inline">Manage Users</h4>
+            <hr className="mb-4"></hr>
             <button type="button" className="btn btn-outline-dark d-inline float-right" onClick={onAddUser}>+</button>
-            <table className="table table-hover">
-                <thead>
-                    <tr>
-                        <th scope="col">Name</th>
-                        <th scope="col">UserName</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <ListUsers users={users} onSelectUser={onSelectUser} onDeleteUser={onDeleteUser} />
-                </tbody>
-            </table>
+            <div class="table-responsive">
+                <table className="table table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">Name</th>
+                            <th scope="col">UserName</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <ListUsers users={users} onSelectUser={onSelectUser} onDeleteUser={onDeleteUser} />
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 }
